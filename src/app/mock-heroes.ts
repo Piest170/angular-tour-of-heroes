3b0ea932231c57def15e48d7c6879e74e4694db0
Piest170 <PU170@hotmail.com>
2023-07-04 19:45:45 +0700

    Push in Git

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,24 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Iron Man' },
+  { id: 2, name: 'Batman' },
+  { id: 3, name: 'Superman' },
+  { id: 4, name: 'Spiderman' },
+  { id: 5, name: 'Captain America' },
+  { id: 6, name: 'Dr Strange' },
+  { id: 7, name: 'Cybrog' },
+  { id: 8, name: 'Wonder Woman' },
+  { id: 9, name: 'The Flash' },
+  { id: 10, name: 'Thor' },
+  { id: 11, name: 'Dr Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
